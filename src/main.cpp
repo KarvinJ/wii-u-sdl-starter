5aa3761a741fe041fbab5178d463469ff934b55d
knight <karvin_jimenez@hotmail.com>
2024-10-05 11:24:45 -0400

    extra changes.

--- src/main.cpp
+++ src/main.cpp
@@ -146,7 +146,7 @@ void update(float deltaTime)
     ball.y += ballVelocityY * deltaTime;
 }
 
-void renderSprite(Sprite sprite)
+void renderSprite(Sprite &sprite)
 {
     SDL_RenderCopy(renderer, sprite.texture, NULL, &sprite.textureBounds);
 }
